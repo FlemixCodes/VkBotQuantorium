19cb189707af28483da6ed5596c7ba197aed0d4f
Flemix Codes <110670573+FlemixCodes@users.noreply.github.com>
2023-02-26 13:59:23 +0500

    Update vk.py

--- vk.py
+++ vk.py
@@ -14,21 +14,20 @@ longpoll = VkLongPoll(session) # Поднятие LongPoll
 print("Успешный запуск бота")
 
 def main():
-    while True:
-        try:
-            for event in longpoll.listen(): # Слушаем сервер
-                if event.type == VkEventType.MESSAGE_NEW: # Пришло событие (новое сообщение)
-                    if event.to_me: # Пришло для нас
-                        message = event.text.lower() # Достаем текст сообщения из события
+    try:
+        for event in longpoll.listen(): # Слушаем сервер
+            if event.type == VkEventType.MESSAGE_NEW: # Пришло событие (новое сообщение)
+                if event.to_me: # Пришло для нас
+                    message = event.text.lower() # Достаем текст сообщения из события
 
-                        if message == "привет":
-                            api.messages.send(peer_id=event.peer_id, message="Привет!", random_id=0)
-                        elif message == "пока":
-                            api.messages.send(peer_id=event.peer_id, message="Пока!", random_id=0)
-                        else:
-                            api.messages.send(peer_id=event.peer_id, message="Я вас не понимаю :(", random_id=0)
-        except Exception as e:
-            print(f"Longpoll Exception: {e}")
+                    if message == "привет":
+                        api.messages.send(peer_id=event.peer_id, message="Привет!", random_id=0)
+                    elif message == "пока":
+                        api.messages.send(peer_id=event.peer_id, message="Пока!", random_id=0)
+                    else:
+                        api.messages.send(peer_id=event.peer_id, message="Я вас не понимаю :(", random_id=0)
+    except Exception as e:
+        print(f"Longpoll Exception: {e}")
 
 if __name__ == "__main__":
     main()
